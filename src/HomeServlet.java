afb4211b556115f2e4f0cb30f0abe16da151c32d
elvinh <eltonv93@gmail.com>
2017-09-26 20:18:45 -0700

    set default landing page

--- src/HomeServlet.java
+++ src/HomeServlet.java
@@ -22,7 +22,7 @@ import org.apache.commons.dbutils.DbUtils;
 /**
  * Servlet implementation class HomeServlet
  */
-@WebServlet("/home/*")
+@WebServlet("/main/*")
 public class HomeServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
