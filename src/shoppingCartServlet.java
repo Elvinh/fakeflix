508205bd01ada6d5f6c7a18afd8b917374998870
elvinh <eltonv93@gmail.com>
2017-10-09 21:56:41 -0700

    Revert "Adding in Delete SC"
    This reverts commit 282db134dfef6728246715a2902bdf5d4a1701e3.

--- src/shoppingCartServlet.java
+++ src/shoppingCartServlet.java
@@ -130,14 +130,14 @@ public class shoppingCartServlet extends HttpServlet {
 				else
 				{
 					System.out.println("I'm over here");
-					PreparedStatement ps = (PreparedStatement) conn.prepareStatement("insert into shoppingcart values ( " + userID + ", " + movieID + ", " + 1 +  ")");
-					ps.execute();
-					movie = new Movies();
-					movie.setQuantity(1);
-					movie.setTitle(movieName);
-					movie.setPrice((float) 1.99);
-					cart.addToCart(movieName, movie);
-					session.setAttribute("cart", cart);
+				PreparedStatement ps = (PreparedStatement) conn.prepareStatement("insert into shoppingcart values ( " + userID + ", " + movieID + ", " + 1 +  ")");
+				ps.execute();
+				movie = new Movies();
+				movie.setQuantity(1);
+				movie.setTitle(movieName);
+				movie.setPrice((float) 1.99);
+				cart.addToCart(movieName, movie);
+				session.setAttribute("cart", cart);
 				}
 				//PreparedStatement stmt=con.prepareStatement("insert into Emp values(?,?)"); 
 				
@@ -156,56 +156,93 @@ public class shoppingCartServlet extends HttpServlet {
 				DbUtils.closeQuietly(st);
 				DbUtils.closeQuietly(conn);
 			}
+			
+		
+		
+			
+			
+			//response.addCookie(c1);
+		
 	    
 	    request.setAttribute("addedItem", movieName);
 	    RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/addedToCart.jsp");
 		dispatcher.forward(request, response);
 	}
+	/*
+	 * <%
+		
+		Cookie[] cookies =  request.getCookies();
+		String name = "";
+		int quantity = 0;
+		int counter = 0;
+		String tempName = "";
+		String tempValue = "";
 	
+		// 1. SC works so far with user logging in and adding movies to cart. 
+		// 1. Correctly takes out the entire list when user logs out
+		// 2. When anon user add movies to list, correctly adds them in
+		// 2. And leaves them in until cookie expires.
+		// SITUATION 3. NEEDS WORK.
+		// 3. Fix situation where user adds in movies to SC, but then logins.
+		// Currently the indexing is messed up. The cookies list is out of order, will either
+		// A. Need to find a formula to fix the indexing
+		// B. When user logs in, place info in the correct slots, then add back the movies to the list.
+		
+		if(cookies != null)
+		{
+			for(int i = 0; i < cookies.length; i++)
+			{
+				System.out.println("Length of Cookies @ SC: " + cookies.length);
+				tempName = cookies[i].getName();
+				tempValue = cookies[i].getValue();
+				
+				%><p><%= tempName %></p>
+				<p><%= tempValue %></p><%
+			}
+			if(cookies[0].getName().equals("loginedUser"))
+			{
+				counter = 4;
+			}
+			else if (cookies[1].getName().equals("loginedUser"))
+			{
+				counter = 4;
+			}
+			else
+			{
+				counter = 0;	
+			}
+			for(int i = counter; i < cookies.length; i++)
+			{
+					//name = cookies[i].getName();
+					//quantity = Integer.parseInt(cookies[i].getValue());
+					%>
+					
+					<tr>
+						<td>
+							<p> <%= name %></p>
+						</td>
+						<td>
+							<p> <%= quantity %></p>
+						</td>
+						<td>
+							<p> 1</p>
+						</td>
+					</tr><% 
+			}
+		}
+			
+		
+		%>
+		
+		
+	 */
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		Connection conn = null;
-		Statement st = null;
-		ResultSet rs = null;
-
-		response.setContentType("text/html");
-		HttpSession session = request.getSession(true);
-		ShoppingCart cart;
-		
-		String url = "jdbc:mysql://localhost:3306/";
-		String db = "moviedb";
-		String driver = "com.mysql.jdbc.Driver";
-		String user = "root";
-		String password = "admin";
-		
-		String movieName = request.getParameter("removeMovie");
-		try
-		{
-			Class.forName(driver);
-			conn = DriverManager.getConnection(url+db, user, password);
-			st = conn.createStatement();
-			
-			if(movieName != null)
-			{
-				rs = st.executeQuery("delete from shoppingcart where in movieid = (select id from movies.id = '" + movieName + "')");
-			}
-		}
-		catch(SQLException | ClassNotFoundException e) 
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} 
-		finally {
-			DbUtils.closeQuietly(rs);
-			DbUtils.closeQuietly(st);
-			DbUtils.closeQuietly(conn);
-		}
-		
-		//doGet(request, response);
+		doGet(request, response);
 	}
 
 }
